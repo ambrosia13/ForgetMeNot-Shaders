c88f8be3ebf1ba9358659bb484a323e3c86ad6d1
Poisoned-Honey <poisonedhoney13@gmail.com>
2022-12-10 18:22:12 -0800

    Customizable season length, modularized some code

--- assets/forgetmenot/shaders/lib/functions/utility.glsl
+++ assets/forgetmenot/shaders/lib/functions/utility.glsl
@@ -171,20 +171,37 @@ vec3 sampleCubemapFaces(samplerCube u_cube) {
     return color;
 }
 
+vec4 getSeasonFactors(out float time) {
+    time = frx_worldDay + frx_worldTime;
+
+    #if STARTING_SEASON == SEASON_SUMMER
+        time += SEASON_LENGTH + TRANSITION_LENGTH;
+    #elif STARTING_SEASON == SEASON_AUTUMN
+        time += SEASON_LENGTH * 2.0 + TRANSITION_LENGTH * 2.0;
+    #elif STARTING_SEASON == SEASON_WINTER
+        time += SEASON_LENGTH * 3.0 + TRANSITION_LENGTH * 3.0;
+    #endif
+
+    time = mod(time, SEASON_LENGTH * 4.0 + TRANSITION_LENGTH * 4.0);
+
+    float toSummer = smoothstep(SEASON_LENGTH, SEASON_LENGTH + TRANSITION_LENGTH, time);
+    float toAutumn = smoothstep(SEASON_LENGTH * 2.0 + TRANSITION_LENGTH, SEASON_LENGTH * 2.0 + TRANSITION_LENGTH * 2.0, time);
+    float toWinter = smoothstep(SEASON_LENGTH * 3.0 + TRANSITION_LENGTH * 2.0, SEASON_LENGTH * 3.0 + TRANSITION_LENGTH * 3.0, time);
+    float toSpring = smoothstep(SEASON_LENGTH * 4.0 + TRANSITION_LENGTH * 3.0, SEASON_LENGTH * 4.0 + TRANSITION_LENGTH * 4.0, time);
+
+    return vec4(toSummer, toAutumn, toWinter, toSpring);
+}
+
 // Foliage and tree color depending on season.
 vec3 getSeasonColor(in vec3 vertexColor, in int isLeafBlock, vec3 worldCoord) {
     #ifdef SEASONS
-        float time = frx_worldDay + frx_worldTime;
+        float time = 0.0;
+        vec4 seasonFactors = getSeasonFactors(time);
 
-        #if STARTING_SEASON == SEASON_SUMMER
-            time += 6.0;
-        #elif STARTING_SEASON == SEASON_AUTUMN
-            time += 12.0;
-        #elif STARTING_SEASON == SEASON_WINTER
-            time += 18.0;
-        #endif
-
-        time = mod(time, 23.0);
+        float toSummer = seasonFactors.x;
+        float toAutumn = seasonFactors.y;
+        float toWinter = seasonFactors.z;
+        float toSpring = seasonFactors.w;
 
         #ifdef VARIED_TREE_COLOR
             float noise = smoothstep(0.4, 0.7, smoothHash(worldCoord.xz * 0.01));
@@ -196,10 +213,11 @@ vec3 getSeasonColor(in vec3 vertexColor, in int isLeafBlock, vec3 worldCoord) {
 
         vertexColor = mix(vertexColor, vec3(1.000,0.592,0.837) * 2.0, leaves * noise);
         vec3 seasonColor = vertexColor;
-        seasonColor = mix(seasonColor, vec3(0.9, 1.0, 0.4), smoothstep(4.0, 5.0, time));   // summer
-        seasonColor = mix(seasonColor, mix(vec3(1.000,0.666,0.346), mix(vec3(1.000,0.480,0.105), vec3(1.000,0.341,0.158) * 1.5, noise), leaves), smoothstep(10.0, 11.0, time)); // autumn
-        seasonColor = mix(seasonColor, mix(vec3(0.828,1.000,0.844), vec3(1.000,0.874,0.912), leaves), smoothstep(16.0, 17.0, time)); // winter
-        seasonColor = mix(seasonColor, vertexColor, smoothstep(22.0, 23.0, time));             // spring
+
+        seasonColor = mix(seasonColor, vec3(0.9, 1.0, 0.4), toSummer); // summer
+        seasonColor = mix(seasonColor, mix(vec3(1.000,0.666,0.346), mix(vec3(1.000,0.480,0.105), vec3(1.000,0.341,0.158) * 1.5, noise), leaves), toAutumn); // autumn
+        seasonColor = mix(seasonColor, mix(vec3(0.828,1.000,0.844), vec3(1.000,0.874,0.912), leaves), toWinter); // winter
+        seasonColor = mix(seasonColor, vertexColor, toSpring); // spring
 
         return seasonColor;
     #else
@@ -210,17 +228,13 @@ vec3 getSeasonColor(in vec3 vertexColor, in int isLeafBlock, vec3 worldCoord) {
 // Threshold for leaves to be discarded based on season.
 float getLeavesFallingThreshold(vec3 worldCoord) {
     #ifdef SEASONS
-        float time = frx_worldDay + frx_worldTime;
+        float time = 0.0;
+        vec4 seasonFactors = getSeasonFactors(time);
 
-        #if STARTING_SEASON == SEASON_SUMMER
-            time += 6.0;
-        #elif STARTING_SEASON == SEASON_AUTUMN
-            time += 12.0;
-        #elif STARTING_SEASON == SEASON_WINTER
-            time += 18.0;
-        #endif
-
-        time = mod(time, 23.0);
+        float toSummer = seasonFactors.x;
+        float toAutumn = seasonFactors.y;
+        float toWinter = seasonFactors.z;
+        float toSpring = seasonFactors.w;
 
         #ifdef VARIED_TREE_COLOR
             float noise = smoothstep(0.4, 0.7, smoothHash(worldCoord.xz * 0.01 + 1000.0));
@@ -229,10 +243,10 @@ float getLeavesFallingThreshold(vec3 worldCoord) {
         #endif
 
         float threshold = 2.0;
-        threshold = mix(threshold, 2.0, smoothstep(4.0, 5.0, time));   // summer
-        threshold = mix(threshold, mix(0.95, 0.9, noise), smoothstep(10.0, 11.0, time)); // autumn
-        threshold = mix(threshold, mix(0.5, 0.4, noise), smoothstep(16.0, 17.0, time)); // winter
-        threshold = mix(threshold, 2.0, smoothstep(22.0, 23.0, time));             // spring
+        threshold = mix(threshold, 2.0, toSummer); // summer
+        threshold = mix(threshold, mix(0.95, 0.9, noise), toAutumn); // autumn
+        threshold = mix(threshold, mix(0.5, 0.4, noise), toWinter); // winter
+        threshold = mix(threshold, 2.0, toSpring); // spring
 
         return threshold;
     #else
@@ -243,23 +257,19 @@ float getLeavesFallingThreshold(vec3 worldCoord) {
 // Season-dependent fog factor. To be added onto original fog factor.
 float getSeasonFogFactor() {
     #ifdef SEASONS
-        float time = frx_worldDay + frx_worldTime;
+        float time = 0.0;
+        vec4 seasonFactors = getSeasonFactors(time);
 
-        #if STARTING_SEASON == SEASON_SUMMER
-            time += 6.0;
-        #elif STARTING_SEASON == SEASON_AUTUMN
-            time += 12.0;
-        #elif STARTING_SEASON == SEASON_WINTER
-            time += 18.0;
-        #endif
-
-        time = mod(time, 23.0);
+        float toSummer = seasonFactors.x;
+        float toAutumn = seasonFactors.y;
+        float toWinter = seasonFactors.z;
+        float toSpring = seasonFactors.w;
 
         float fog = 0.25;
-        fog = mix(fog, 0.0, smoothstep(4.0, 5.0, time));   // summer
-        fog = mix(fog, 0.25, smoothstep(10.0, 11.0, time)); // autumn
-        fog = mix(fog, 0.75, smoothstep(16.0, 17.0, time)); // winter
-        fog = mix(fog, 0.25, smoothstep(22.0, 23.0, time)); // spring
+        fog = mix(fog, 0.0, toSummer);   // summer
+        fog = mix(fog, 0.25, toAutumn); // autumn
+        fog = mix(fog, 0.75, toWinter); // winter
+        fog = mix(fog, 0.25, toSpring); // spring
 
         return fog * (1.0 - sqrt(max(0.0001, getSunVector().y)));
     #else
@@ -269,23 +279,19 @@ float getSeasonFogFactor() {
 
 float getSeasonCloudsFactor() {
     #ifdef SEASONS
-        float time = frx_worldDay + frx_worldTime;
+        float time = 0.0;
+        vec4 seasonFactors = getSeasonFactors(time);
 
-        #if STARTING_SEASON == SEASON_SUMMER
-            time += 6.0;
-        #elif STARTING_SEASON == SEASON_AUTUMN
-            time += 12.0;
-        #elif STARTING_SEASON == SEASON_WINTER
-            time += 18.0;
-        #endif
-
-        time = mod(time, 23.0);
+        float toSummer = seasonFactors.x;
+        float toAutumn = seasonFactors.y;
+        float toWinter = seasonFactors.z;
+        float toSpring = seasonFactors.w;
 
         float lowerBound = 0.4;
-        lowerBound = mix(lowerBound, 0.55, smoothstep(4.0, 5.0, time));   // summer
-        lowerBound = mix(lowerBound, 0.35, smoothstep(10.0, 11.0, time)); // autumn
-        lowerBound = mix(lowerBound, 0.2, smoothstep(16.0, 17.0, time)); // winter
-        lowerBound = mix(lowerBound, 0.4, smoothstep(22.0, 23.0, time)); // spring
+        lowerBound = mix(lowerBound, 0.55, toSummer);   // summer
+        lowerBound = mix(lowerBound, 0.35, toAutumn); // autumn
+        lowerBound = mix(lowerBound, 0.20, toWinter); // winter
+        lowerBound = mix(lowerBound, 0.40, toSpring); // spring
 
         return lowerBound;
     #else
--- assets/forgetmenot/shaders/post/fabulous/depth_downsample.frag
+++ assets/forgetmenot/shaders/post/fabulous/depth_downsample.frag
@@ -7,6 +7,11 @@ in vec2 texcoord;
 layout(location = 0) out vec4 fragColor;
 
 void main() {
-    fragColor.r = textureLod(u_depth, texcoord, max(0, frxu_lod - 1)).r;
-    fragColor.gba = vec3(0.0, 0.0, 1.0);
+    // fragColor = vec4(1.0);
+    // for(int i = -1; i <= 1; i++) {
+    //     for(int j = -1; j <= 1; j++) {
+    //         fragColor = min(fragColor, textureLod(u_depth, texcoord + vec2(i, j) / frxu_size, max(0, frxu_lod - 1)));
+    //     }
+    // }
+    fragColor = textureLod(u_depth, texcoord, max(0, frxu_lod - 1));
 }
