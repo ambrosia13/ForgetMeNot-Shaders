1d40f6d90703226e9cf6599d91a6fb410435668b
fewizz <hazeevaidar@gmail.com>
2023-03-20 08:02:34 +0300

    Simplify traverser

--- assets/forgetmenot/shaders/lib/inc/raytrace.glsl
+++ assets/forgetmenot/shaders/lib/inc/raytrace.glsl
@@ -4,88 +4,37 @@
 Contains the raytracer from lomo, provided by fewizz.
 */
 
-vec3 cam_dir_to_win(vec3 pos_cs, vec3 dir_cs, mat4 projMat) {
-	vec4 p = vec4(pos_cs, 1.);
-	vec4 n = vec4(dir_cs, 0.);
-
-	vec4 X = (projMat * (p + n));
-	vec4 Y = (projMat * p);
-
-	return normalize(
-		vec3(frxu_size, 1.0) *
-		((X.xyz / X.w) - (Y.xyz / Y.w))
-	);
-}
-
 const uint power_of_two = 2u;
 const uint last_level = 8u;
 
-uint cell_size(uint level) { return 1u << level; }
-float cell_size_f(uint level) { return float(cell_size(level)); }
-uint cell_mask(uint level) { return cell_size(level) - 1u; }
-
-float position_in_cell(uint texel, float inner, uint level) {
-	return float(texel & cell_mask(level)) + inner;
-}
-
-float dist_negative(uint texel, float inner, uint level) {
-	return position_in_cell(texel, inner, level);
-}
-
-float dist_positive(uint texel, float inner, uint level) {
-	return cell_size_f(level) - dist_negative(texel, inner, level);
-}
-
-float dist_to_axis(uint texel0, float inner0, uint level, float dir0) {
-	return mix(dist_negative(texel0, inner0, level), dist_positive(texel0, inner0, level), step(0.0, dir0));
-}
-
 float next_cell(inout uvec2 texel, inout vec2 inner, vec2 dir, uint level) {
-	vec2 dists_to_axis = vec2(
-		dist_to_axis(texel.x, inner.x, level, dir.x),
-		dist_to_axis(texel.y, inner.y, level, dir.y)
-	);
-	vec2 diagonal_dists = dists_to_axis / abs(dir);
+	uint cell_size = 1u << level;
+	vec2 distance_in_cell = (texel & (cell_size - 1u)) + inner;
 
-	vec2 dir_signs = sign(dir);
+	vec2 dists_to_axis = cell_size - distance_in_cell;
+	vec2 diagonal_dists = dists_to_axis / dir;
 
 	if(diagonal_dists.x < diagonal_dists.y) {
-		texel.x -= texel.x & cell_mask(level);
-
-		if(dir_signs.x > 0.0) {
-			texel.x += cell_size(level);
-			inner.x = 0.0;
-		} else {
-			texel.x -= 1u;
-			inner.x = 1.0;
-		}
+		texel.x = ((texel.x >> level) + 1u) << level;
+		inner.x = 0.0;
 
 		float y = inner.y + dir.y * diagonal_dists.x;
-		inner.y = fract(y);
-		texel.y += uint(int(floor(y)));
+		uint uy = uint(y);
+		inner.y = y - uy;
+		texel.y += uy;
+
 		return diagonal_dists.x;
 	} else {
-		texel.y -= texel.y & cell_mask(level);
-
-		if(dir_signs.y > 0.0) {
-			texel.y += cell_size(level);
-			inner.y = 0.0;
-		} else {
-			texel.y -= 1u;
-			inner.y = 1.0;
-		}
+		texel.y = ((texel.y >> level) + 1u) << level;
+		inner.y = 0.0;
 
 		float x = inner.x + dir.x * diagonal_dists.y;
-		inner.x = fract(x);
-		texel.x += uint(int(floor(x)));
+		uint ux = uint(x);
+		inner.x = x - ux;
+		texel.x += ux;
+
 		return diagonal_dists.y;
 	}
-}
-
-bool is_out_of_fb(uvec2 texel, float z) {
-	return
-		any(greaterThanEqual(texel, uvec2(frxu_size))) ||
-		z <= 0.0;
 }
 
 bool raytrace(in vec3 pos_win, in vec3 dir_ws, in int steps, in sampler2D depths, out vec3 hitPos) {
@@ -96,46 +45,51 @@ bool raytrace(in vec3 pos_win, in vec3 dir_ws, in int steps, in sampler2D depths
 
 	float dir_ws_xy_length = length(dir_ws.xy);
 	vec2 dir_xy = dir_ws.xy / dir_ws_xy_length;
+	vec2 sgn_xy = sign(dir_xy);
+	vec2 dir_xy_abs = dir_xy * sgn_xy;
+	ivec2 isgn_xy = ivec2(sgn_xy);
 
-	uvec2 texel = uvec2(pos_win.xy);
-	vec2 inner = vec2(fract(pos_win.xy));
+	uvec2 texel = uvec2(ivec2(pos_win.xy * sgn_xy));
+	vec2 inner = fract(pos_win.xy * sgn_xy);
 	float z = pos_win.z;
 
-	while(true) {
-		if(steps == 0 || is_out_of_fb(texel, z)) {
-			break;
-		}
+	while(
+		steps > 0 &&
+		all(lessThan(uvec2(ivec2(texel) * isgn_xy), frxu_size)) &&
+		z > 0.0
+	) {
 		--steps;
 
 		uint level = last_level;
-		float lower_depth = texelFetch(depths, ivec2(texel >> int(last_level)), int(last_level)).r;
+		ivec2 itexel = ivec2(texel) * isgn_xy;
+		float lower_depth = texelFetch(depths, itexel >> last_level, int(last_level)).r;
 
 		while(z >= lower_depth && level > 0u) {
 			level -= power_of_two;
-			lower_depth = texelFetch(depths, ivec2(texel >> int(level)), int(level)).r;
+			lower_depth = texelFetch(depths, itexel >> level, int(level)).r;
 		}
 
 		uvec2 prev_texel = texel;
 		vec2 prev_inner = inner;
 		float prev_z = z;
-		float dist_xy = next_cell(texel, inner, dir_xy, level);
+		float dist_xy = next_cell(texel, inner, dir_xy_abs, level);
 		z += dist_xy * (dir_ws.z / dir_ws_xy_length);
 
 		if(z >= lower_depth) {
 			if(level == 0u) {
-				hitPos.xy = (vec2(prev_texel) + 0.5) / frxu_size;
+				hitPos.xy = (vec2(ivec2(prev_texel) * isgn_xy) + 0.5) / frxu_size;
 				hitPos.z = mix(lower_depth, prev_z, step(lower_depth, prev_z));
 
-				
 				return hitPos.z < 1.0 && abs(linearizeDepth(hitPos.z) - linearizeDepth(lower_depth)) < mix(2.0, 10.0, linearstep(10.0, 40.0, linearDepth));
 			}
+
 			float mul = (lower_depth - prev_z) / (z - prev_z);
 			dist_xy *= mul;
 
-			vec2 diff = prev_inner + dist_xy * dir_xy;
-
-			inner = fract(diff);
-			texel = prev_texel + uvec2(ivec2(floor(diff)));
+			vec2 diff = prev_inner + dist_xy * dir_xy_abs;
+			uvec2 udiff = uvec2(diff);
+			inner = diff - udiff;
+			texel = prev_texel + udiff;
 
 			z = lower_depth;
 		}
