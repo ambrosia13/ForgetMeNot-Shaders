b3c0b249f4c53d88629250a59561e427c90cf5bf
Poisoned-Honey <poisonedhoney13@gmail.com>
2023-10-28 11:38:54 -0700

    Brighter water color

--- assets/forgetmenot/shaders/lib/materials_pipeline_common.glsl
+++ assets/forgetmenot/shaders/lib/materials_pipeline_common.glsl
@@ -16,5 +16,5 @@ Common things that should be available to both pipeline and material shaders.
 #include forgetmenot:seasons
 #include forgetmenot:misc
 
-#define WATER_COLOR (vec3(WATER_COLOR_R, WATER_COLOR_G, WATER_COLOR_B))
+#define WATER_COLOR (vec3(WATER_COLOR_R, WATER_COLOR_G, WATER_COLOR_B) * 1.5)
 #define END_MIST_COLOR (vec3(END_MIST_COLOR_R, END_MIST_COLOR_G, END_MIST_COLOR_B))
